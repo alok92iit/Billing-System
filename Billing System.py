0ed700cbca5dd6f0ecd91f99417640daade7f8e0
Alok Sharma <alok92ii@gmail.com>
2020-09-30 12:33:22 +0530

    1. Combine customer detail form in a frame colled Customer Details..(or frame1) to make attractive GUI 2. Merge the wintow to enter product details into sec window for decresing complexity for user. 3. Creat second frame which will contains products details .Adjust both frames ,means when we change the size of frame size automatically change . 4. Add scroll bar in products details.. frame so that user can easly see the product details . 5. Adjust entry wedget in grid fashion for entering product details and also adjust heading of grid column with the help label6. Increase the size of window when Ok button is clicked and then make it inactive'

--- Billing System.py
+++ Billing System.py
@@ -1,84 +1,108 @@
 from tkinter import *
-global pq
 def genrateBill():                    #second window take user detail
-     
+    global sec 
     sec= Toplevel(window )
     
-    sec.geometry("1536x864")
+    sec.geometry("800x250")
+    frame1=LabelFrame(sec  ,text="Customer  Details.. " , padx=10 ,pady=10  )
+    frame1.pack(padx=10 ,pady= 10 ,fill=BOTH )
     
-    name=Label(sec,text="Enter Custumer Name")
+    name=Label(frame1,text="Enter Custumer Name")
     name.grid(row="0" ,column="0",padx="20" ,pady="20")
     
     
-    nameBox=Entry(sec ,textvariable= Name)
+    nameBox=Entry(frame1,textvariable= Name)
     nameBox.grid(row="0" , column="1")
 
      
-    email=Label(sec,text="Enter Email")
+    email=Label(frame1,text="Enter Email")
     email.grid(row="1" ,column="0",padx="0")
 
-    emailBox=Entry(sec ,textvariable=EmailId)
+    emailBox=Entry(frame1 ,textvariable=EmailId)
     emailBox.grid(row="1" ,column="1")
     
     
-    mobile=Label(sec,text="Enter Mobile No")
+    mobile=Label(frame1,text="Enter Mobile No")
     mobile.grid(row="0 ",column="2" ,padx="20")
     
-    mobileBox=Entry(sec,textvariable=mobno)
+    mobileBox=Entry(frame1,textvariable=mobno)
     mobileBox.grid(row="0" ,column ="3" )
     
 
-    address=Label(sec, text="Enter Addess")
+    address=Label(frame1, text="Enter Addess")
     address.grid(row="1" ,column="2" ,padx ="20")
     
-    addressBox=Entry(sec ,textvariable=Address)
+    addressBox=Entry(frame1 ,textvariable=Address)
     addressBox.grid(row="1" ,column="3")
 
     
-    items=Label(sec, text="No Of Items")
+    items=Label(frame1, text="No Of Items")
     items.grid(row =2 ,column =0 ,pady=(15 ,0))
     
-    itemBox=Scale(sec ,from_ =1 ,to=100 ,orient =HORIZONTAL ,length =200 )
+    #slider which will take number of products as a input
+    
+    global itemBox
+ 
+    itemBox=Scale(frame1 ,from_ =1 ,to=100 ,orient =HORIZONTAL ,length =200 )
     itemBox.grid(row =2 ,column= 1)
     #p= itemBox.get()
     
-    
-    genrate=Button(sec,text="Genrate Bill" ,command=insertData)   #button to insert items 
-    genrate.place(x="260" ,y="150")
+    global ok
+    ok=Button(frame1,text="Ok" ,command=insertData ,padx=25 ,width =20)   #button to insert items 
+    ok.grid(row=2,column=2 , pady=(15,0),padx=(35,0) )
 
-def insertData():                          #Window that take input of purchased item data
-    insertwin =Toplevel() 
-    insertwin.geometry('{}x{}'.format(insertwin.winfo_screenwidth(),insertwin.winfo_screenheight()))
-    #this section only show the custimer details
-    
-    n=Label(insertwin,text="Name :",font="22px")
-    n.grid(row="0", column ="0",sticky =W , padx=(50 ,0) ,pady =(50,0))
 
-    c=Name.get()
-    x=Label(insertwin,text=c,font="22px" )
-    x.grid(row="0", column ="1",pady =(50,0) )
+def insertData():
+    ok.config(state=DISABLED)  #confif() to change coniguration of ok button to Disable ok button of frame1
+    sec.geometry('{}x{}'.format(sec.winfo_screenwidth(),sec.winfo_screenheight())) #create window of monitor size
+    c=itemBox.get()
     
-    y1=Label(insertwin,text="Mobile No :",font="22px")
-    y1.grid(row="0", column ="2", padx=(50 ,0) ,pady =(50,0))
-    y=mobno.get()
-    y2=Label(insertwin,text=y,font="22px" )
-    y2.grid(row="0", column ="3",sticky =W,pady =(50,0) )
+    frame=LabelFrame(sec  ,text="products Details.. " ) #Box inner padding
+    frame.pack(padx=10 ,pady= 10 ,fill=BOTH ,expand=1)
     
-    z=Label(insertwin,text="Address :",font="22px")
-    z.grid(row="1", column ="0",sticky =W , padx=(50 ,0) ,pady =(50,0))
-    z1=Address.get()
-    z2=Label(insertwin,text=z1,font="22px" )
-    z2.grid(row="1", column ="1",sticky =W ,pady =(50,0) )
+    #for adding scroll bar in frame wedget
     
-    q=Label(insertwin,text="Email Id :",font="22px")
-    q.grid(row="1", column ="2",sticky =W , padx=(50 ,0) ,pady =(50,0))
-    q1=EmailId.get()
-    q2=Label(insertwin,text=q1,font="22px" )
-    q2.grid(row="1", column ="3",sticky =W ,pady =(50,0) )
+    my_canvas=Canvas(frame)
+    my_canvas.pack(side=LEFT,fill=BOTH ,expand =1)
+
+    scroll=Scrollbar(frame ,orient =VERTICAL ,command =my_canvas.yview)
+    scroll.pack(fill=Y ,side=RIGHT)
+
+    my_canvas.configure(yscrollcommand=scroll.set)
+    my_canvas.bind('<Configure>',lambda e: my_canvas.configure(scrollregion=my_canvas.bbox("all")))
+    second_frame=Frame(my_canvas)
+
+    my_canvas.create_window((0,0) ,window= second_frame )
     
+    # Heading of the table field 
     
-     #this section only show the custimer details
+    sn0=Label(second_frame ,text ="S.No",font='Times 12 bold').grid(row =0 ,column= 0)
+    product=Label(second_frame  ,text="Product Name",font='Times 12 bold').grid(row=0 ,column=1)
+    quantity=Label(second_frame , text="Quantity \n (eg:1gm,1kg,1ps)",font='Times 12 bold').grid(row=0,column=2)
+    price =Label(second_frame , text="Price" ,font='Times 12 bold').grid(row =0 ,column=3)
+    discount =Label(second_frame , text="Discount",font='Times 12 bold').grid(row=0 ,column=4)
+    tax =Label(second_frame , text="Tax( in %)" ,font='Times 12 bold').grid(row=0 ,column=5)
     
+   
+    l=[]
+    
+    # Align Entry wedget in grid fashion
+    
+    for i in range(c):
+        l.append([])
+        
+        for j in range(5):
+            
+            #This x label object will print serial no before each row . 
+            
+            x=Label(second_frame ,text=i+1,font='Times 12 bold',padx="5").grid(row=i+1 ,column=0 )  
+            
+            emailBox=Entry(second_frame ,width=30 )
+            emailBox.grid(row=i+1 ,column=j+1 , pady=5,padx=5   )
+            l[i].append(emailBox)  
+    genrate=Button(second_frame,text="Genrate Bill"  ,padx=5 ,width =20)   #button to show final detail to user for further modification
+    genrate.grid(row=c+1,column= 3, pady=15,padx=15 )
+
     
 window= Tk()                             # Main window of application
 Name=StringVar()
