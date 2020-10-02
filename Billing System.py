b58aec3ff2d4dd593af04448fbac2ddb563b3b8b
Alok Sharma <alok92ii@gmail.com>
2020-10-02 15:45:34 +0530

    1 Remove multiple declaration of same labels. 2 Improve form  validation  .

--- Billing System.py
+++ Billing System.py
@@ -1,40 +1,55 @@
 from tkinter import *
-#from tkinter import messagebox
+from tkinter import messagebox
+
+fontL ='Times 18'        #font style for labels
+fontE ='Times 16'        #font style for entry wedget
+
+#Contains all form labels   
+
+def labels(frameobject):
+     
+    frame=frameobject
+    
+    n=Label(frame,text="Name :",font= fontL)
+    n.grid(row="0", column ="0",sticky =W , padx=25 ,pady =10)
+    
+    y1=Label(frame,text="Mobile No :",font= fontL)
+    y1.grid(row="1", column ="0", padx=25 ,pady =(0,10))
+
+    z=Label(frame,text="Address :",font= fontL)
+    z.grid(row="1", column ="2",sticky =W , padx=25 ,pady =(0,10))
+    
+    q=Label(frame,text="Email Id :",font= fontL)
+    q.grid(row="0", column ="2",sticky =W , padx=25, pady =10)
 def show():
     #Window that will show the data
     
     showWin =Toplevel() 
     showWin.geometry('{}x{}'.format(showWin.winfo_screenwidth(),showWin.winfo_screenheight()))
     #this section only show the customer details
-    
+    global buyerData
     buyerData =LabelFrame(showWin, text="Consumer Details..")
-    buyerData.pack(fill= BOTH )
+    buyerData.pack(fill= BOTH ,pady =10 ,padx=10 )
     
-    n=Label(buyerData,text="Name :",font='Times 18 bold italic')
-    n.grid(row="0", column ="0",sticky =W , padx=25 ,pady =(50,0))
+    labels(buyerData)
 
-    c=Name.get()
+    c=Name.get().strip()
+    x=Label(buyerData,text=c.title() ,font= fontL )
+    x.grid(row="0", column ="1", pady =10)
     
-    x=Label(buyerData,text=c.title() ,font='Times 18 bold italic' )
-    x.grid(row="0", column ="1",pady =(50,0) )
-    
-    y1=Label(buyerData,text="Mobile No :",font='Times 18 bold italic')
-    y1.grid(row="1", column ="0", padx=25 ,pady =(50,0))
+
     y=mobno.get()
-    y2=Label(buyerData,text=y,font='Times 18 bold italic')
-    y2.grid(row="1", column ="1",sticky =W,pady =(50,0) )
+    y2=Label(buyerData,text=y,font= fontL)
+    y2.grid(row="1", column ="1",sticky =W ,pady =(0,10) )
     
-    z=Label(buyerData,text="Address :",font='Times 18 bold italic')
-    z.grid(row="1", column ="2",sticky =W , padx=25 ,pady =(50,0))
     z1=Address.get()
-    z2=Label(buyerData,text=z1.upper(),font='Times 18 bold italic')
-    z2.grid(row="1", column ="3",sticky =W ,pady =(50,0) )
+    z2=Label(buyerData,text=z1.upper(),font= fontL)
+    z2.grid(row="1", column ="3",sticky =W ,pady =(0,10) )
     
-    q=Label(buyerData,text="Email Id :",font='Times 18 bold italic')
-    q.grid(row="0", column ="2",sticky =W , padx=(50 ,0) ,pady =(50,0))
+
     q1=EmailId.get()
-    q2=Label(buyerData,text=q1,font='Times 18 bold italic' )
-    q2.grid(row="0", column ="3",sticky =W ,pady =(50,0) )
+    q2=Label(buyerData,text=q1,font= fontL)
+    q2.grid(row="0", column ="3",sticky =W ,pady =10 )
     
          #this section only show the custimer details
 
@@ -42,72 +57,55 @@ def genrateBill():   #second window take user detail
     global sec 
     sec= Toplevel(window )
     
-    sec.geometry("800x250")
-    frame1=LabelFrame(sec  ,text="Customer  Details.. " , padx=10 ,pady=10  )
-    frame1.pack(padx=10 ,pady= 10 ,fill=BOTH )
+    sec.geometry("850x225")
+    global frame1
+    frame1=LabelFrame(sec  ,text="Customer  Details.. " , padx=10   )
+    frame1.pack(fill=BOTH , padx=10,pady =10)
     
-    name=Label(frame1,text="Enter Custumer Name")
-    name.grid(row="0" ,column="0",padx="20" ,pady="20")
+    labels(frame1)
     
-    nameBox=Entry(frame1,textvariable= Name)
+    nameBox=Entry(frame1,textvariable= Name ,font= fontE)
     nameBox.grid(row="0" , column="1")
     
 
-     
-    email=Label(frame1,text="Enter Email")
-    email.grid(row="1" ,column="0",padx="0")
-
-    emailBox=Entry(frame1 ,textvariable=EmailId)
+    emailBox=Entry(frame1 ,textvariable=EmailId ,font= fontE)
     emailBox.grid(row="1" ,column="1")
-    
-    
-    mobile=Label(frame1,text="Enter Mobile No")
-    mobile.grid(row="0 ",column="2" ,padx="20")
-    
-    mobileBox=Entry(frame1,textvariable=mobno)
+       
+    mobileBox=Entry(frame1,textvariable=mobno ,font= fontE)
     mobileBox.grid(row="0" ,column ="3" )
-    
-
-    address=Label(frame1, text="Enter Address")
-    address.grid(row="1" ,column="2" ,padx ="20")
-    
-    addressBox=Entry(frame1 ,textvariable=Address)
+        
+    addressBox=Entry(frame1 ,textvariable=Address ,font= fontE)
     addressBox.grid(row="1" ,column="3")
 
     
-    items=Label(frame1, text="No Of Items")
-    items.grid(row =2 ,column =0 ,pady=(15 ,0))
+    items=Label(frame1, text="No Of Items" ,font= fontL)
+    items.grid(row =2 ,column =0 , padx=25 ,pady =(0,10))
     
     #slider which will take number of products as a input
     
     global itemBox
  
-    itemBox=Scale(frame1 ,from_ =1 ,to=100 ,orient =HORIZONTAL ,length =200 )
-    itemBox.grid(row =2 ,column= 1)
+    itemBox=Scale(frame1 ,from_ =1 ,to=100 ,orient =HORIZONTAL ,length =200 ,font= fontE)
+    itemBox.grid(row =2 ,column= 1 ,pady=(0,10))
     
     global ok
-    ok=Button(frame1,text="Ok" ,command=insertData ,padx=25 ,width =20)   #button to insert items 
-    ok.grid(row=2,column=2 , pady=(15,0),padx=(35,0) )
+    ok=Button(frame1,text="Ok" ,command=insertData ,padx=25 ,width =10, font= 'Times 12')   #button to insert items 
+    ok.grid(row=2,column=2 , pady=10,padx=(35,0) )
     
 def insertData():
-    name=Name.get()
+    name=Name.get().strip()
     mobileno =mobno.get()
     if(len(name) <= 2):
         messagebox.showwarning("Alert" ,"Please Enter Valid Name")
     
     elif(len(mobileno) == 0 or len(mobileno)==10):
-          
-        if(mobileno[0] not in ['7' ,'8', '9']):
-            
-            messagebox.showwarning("Alert" ,"Please enter valid mobile number")
-            
-        else:    
+    
             ok.config(state=DISABLED)  #config() to change coniguration of ok button to Disable ok button of frame1
             sec.geometry('{}x{}'.format(sec.winfo_screenwidth(),sec.winfo_screenheight())) #create window of monitor size
             c=itemBox.get()
 
             frame=LabelFrame(sec  ,text="products Details.. " ) #Box inner padding
-            frame.pack(padx=10 ,pady= 10 ,fill=BOTH ,expand=1)
+            frame.pack(padx=10 ,pady= 20 ,fill=BOTH ,expand=1)
 
             #for adding scroll bar in frame wedget
 
