f4d0077a177ef5ddb003efb270d984d3f34cf999
Alok Sharma <alok92ii@gmail.com>
2020-10-04 11:36:21 +0530

    1. Display products details in the table using treeview wedget . 2. Validate the product detail frame .

--- Billing System.py
+++ Billing System.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
 fontL ='Times 18'        #font style for labels
 fontE ='Times 16'        #font style for entry wedget
@@ -21,18 +22,10 @@ def labels(frameobject):
     
     q=Label(frame,text="Email Id :",font= fontL)
     q.grid(row="0", column ="2",sticky =W , padx=25, pady =10)
-def show():
-    #Window that will show the data
-    
-    showWin =Toplevel() 
-    showWin.geometry('{}x{}'.format(showWin.winfo_screenwidth(),showWin.winfo_screenheight()))
-    #this section only show the customer details
-    global buyerData
-    buyerData =LabelFrame(showWin, text="Consumer Details..")
-    buyerData.pack(fill= BOTH ,pady =10 ,padx=10 )
-    
-    labels(buyerData)
 
+def labelUserData(frameName):
+    frame =frameName
+    
     c=Name.get().strip()
     x=Label(buyerData,text=c.title() ,font= fontL )
     x.grid(row="0", column ="1", pady =10)
@@ -50,8 +43,65 @@ def show():
     q1=EmailId.get()
     q2=Label(buyerData,text=q1,font= fontL)
     q2.grid(row="0", column ="3",sticky =W ,pady =10 )
+
     
-         #this section only show the custimer details
+def show(): #third window
+    #Window that will show the data
+    temp=0
+    global itemsno
+    itemsno=itemBox.get()
+    l= [[grid[i][j].get() for j in range(5)] for i in range(itemsno)]
+    for i in range(itemsno):
+        if l[i][0]=='' or l[i][1]=='' or l[i][2]=='' or l[i][0].isnumeric():
+                messagebox.showinfo('Alert','You miss somthing in row number {} .\n\t\tOr\n Make some mistake product name'.format(i+1) )
+        elif l[i][2].isnumeric()==False:
+            messagebox.showinfo('Alert','Please enter correct price of product in row {}'.format(i+1))
+        else:
+            temp+=1
+    if temp==itemsno:        
+            showWin =Toplevel() 
+            showWin.geometry('{}x{}'.format(showWin.winfo_screenwidth(),showWin.winfo_screenheight()))
+            #this section only show the customer details
+            global buyerData
+            buyerData =LabelFrame(showWin, text="Consumer Details..")
+            buyerData.pack(fill= BOTH ,pady =10 ,padx=10 )
+        
+            labels(buyerData)          #Call fuction that display label(eg Name Mobile no ..)
+            labelUserData(buyerData)   #Call fuction that display user data enter in entry wedget
+
+            productData=LabelFrame(showWin ,text="Product Details..")
+            productData.pack(fill=BOTH ,pady=10 ,padx=10)
+
+            my_treeview =ttk.Treeview(productData )
+            #define columns 
+            my_treeview['columns'] =('Product Name','Quantity','Price', 'Discount','Tax','Total')
+            #formatting the tree
+
+            my_treeview.column('#0',width=50,anchor=W)     #Phantom column(extra column)
+            my_treeview.column('Product Name' ,width=120 ,anchor=W)  
+            my_treeview.column('Quantity', width=120 ,anchor=W)
+            my_treeview.column('Price' ,width=120 ,anchor=CENTER)
+            my_treeview.column('Discount' ,width=120 ,anchor=W)
+            my_treeview.column('Tax' ,width=120 , anchor=W)
+            my_treeview.column('Total' ,width=120 ,anchor=W)
+
+            my_treeview.heading('#0',text='S.No',anchor=W)
+            my_treeview.heading('Product Name',text='Product Name' ,anchor=W)
+            my_treeview.heading('Quantity',text='Quantity',anchor=W)
+            my_treeview.heading('Price' ,text='Price',anchor=CENTER)
+            my_treeview.heading('Discount' ,text='Discount' ,anchor=W)
+            my_treeview.heading('Tax' ,text='Tax' ,anchor=W)
+            my_treeview.heading('Total',text='Total' ,anchor=W)
+
+
+            sno=1
+            for row  in l:
+
+                my_treeview.insert(parent='' ,index='end',iid = row, text=sno ,
+                                   values=(row[0], row[1], row[2] ,row[3],row[4]))
+                sno +=1
+            my_treeview.pack(fill=BOTH ) 
+    #this section only show the custimer details
 
 def genrateBill():   #second window take user detail
     global sec 
@@ -102,6 +152,7 @@ def insertData():
     
             ok.config(state=DISABLED)  #config() to change coniguration of ok button to Disable ok button of frame1
             sec.geometry('{}x{}'.format(sec.winfo_screenwidth(),sec.winfo_screenheight())) #create window of monitor size
+            
             c=itemBox.get()
 
             frame=LabelFrame(sec  ,text="products Details.. " ) #Box inner padding
@@ -126,17 +177,17 @@ def insertData():
             sn0=Label(second_frame ,text ="S.No",font='Times 12 bold').grid(row =0 ,column= 0)
             product=Label(second_frame  ,text="Product Name",font='Times 12 bold').grid(row=0 ,column=1)
             quantity=Label(second_frame , text="Quantity \n (eg:1gm,1kg,1ps)",font='Times 12 bold').grid(row=0,column=2)
-            price =Label(second_frame , text="Price" ,font='Times 12 bold').grid(row =0 ,column=3)
+            price =Label(second_frame , text="Price \n (per" ,font='Times 12 bold').grid(row =0 ,column=3)
             discount =Label(second_frame , text="Discount",font='Times 12 bold').grid(row=0 ,column=4)
             tax =Label(second_frame , text="Tax( in %)" ,font='Times 12 bold').grid(row=0 ,column=5)
-
-
-            l=[]
+            
+            global grid
+            grid=[]
 
             # Align Entry wedget in grid fashion
 
             for i in range(c):
-                l.append([])
+                grid.append([])
 
                 for j in range(5):
 
@@ -146,9 +197,10 @@ def insertData():
 
                     emailBox=Entry(second_frame ,width=30 )
                     emailBox.grid(row=i+1 ,column=j+1 , pady=5,padx=5   )
-                    l[i].append(emailBox)  
+                    grid[i].append(emailBox)  
             genrate=Button(second_frame,text="Genrate Bill" ,command =show ,padx=5 ,width =20)   #button to insert items 
             genrate.grid(row=c+1,column= 3, pady=15,padx=15 )
+            
     else:
         messagebox.showwarning("Alert" ,"Oops you enter wrong mobile number")
 
